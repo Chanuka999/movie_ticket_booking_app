8368a5e8b3de21f9ec193b659d1e5cf34a62d8eb
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-08 18:02:36 +0530

    feat: add HeroSection component for movie booking interface

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import backgroundImage from "../assets/backgroundImage.png";
+import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <img src={assets.LogoIcon} alt="" className="max-h-21 lg:h-21 mt-20" />
+      <h1 className="text-5xl md:text-[70px] md:leading-18 font-bold max-w-110 text-green-300">
+        Game Of
+        <br />
+        Throns
+      </h1>
+
+      <div className="flex items-center gap-4 text-gray-300">
+        <span>Action | Adventure | Sci-Fi</span>
+        <div className="flex items-center gap-1">
+          <CalendarIcon className="w-4.5 h-4.5" />
+          2018
+        </div>
+        <div className="flex items-center gap-1">
+          <ClockIcon className="w-4.5 h-4.5" />
+          2h 8m
+        </div>
+      </div>
+      <p className="max-w-md text-grey-300">
+        Discover and book your favorite movies in just a few clicks! Explore the
+        latest releases, watch trailers, check showtimes, and reserve your seats
+        instantly. Enjoy a seamless and secure movie ticket booking experience
+        anytime, anywhere.
+      </p>
+      <button
+        onClick={() => navigate("/movies")}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounnded-full font-medium cursor-pointer "
+      >
+        Explore Movies
+        <ArrowRight className="w-5 h-5" />
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
