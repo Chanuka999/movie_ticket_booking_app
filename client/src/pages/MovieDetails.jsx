f1e70e1b19c87407190f8f980ecace9c3e0c0e04
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-12 15:02:23 +0530

    feat: enhance MovieDetails component with loading state and related movies display

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import BlurCircle from "../components/BlurCircle";
 import timeFormat from "../lib/timeFormat.js";
 import Dateselect from "../components/Dateselect.jsx";
+import MovieCard from "../components/MovieCard.jsx";
+import Loading from "../components/Loading.jsx";
 
 const MovieDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [show, setShow] = useState(null);
 
-  useEffect(() => {
-    const getShow = async () => {
-      const show = dummyShowsData.find((show) => show._id === id);
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    if (show) {
       setShow({ movie: show, dateTime: dummyDateTimeData });
-    };
+    }
+  };
 
+  useEffect(() => {
     getShow();
   }, [id]);
 
@@ -84,9 +89,27 @@ const MovieDetails = () => {
       </div>
 
       <Dateselect dateTime={show.dateTime} id={id} />
+
+      <p className="text-lg font-medium mt-20 mb-8">You may also like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.slice(0, 4).map((movie, index) => (
+          <MovieCard key={index} movie={movie} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            scrollTo(0, 0);
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+        >
+          Show more
+        </button>
+      </div>
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loading />
   );
 };
 
