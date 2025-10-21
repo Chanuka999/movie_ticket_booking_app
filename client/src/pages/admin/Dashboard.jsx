d73b139599818e6dd75ee11a135a548efb35ee9b
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-21 22:34:47 +0530

    feat: implement AdminSidebar and Dashboard components with navigation links and data cards

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import {
+  ChartLineIcon,
+  CircleDollarSignIcon,
+  PlayCircleIcon,
+  UsersIcon,
+} from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { dummyDashboardData } from "../../assets/assets";
 
 const Dashboard = () => {
-  return <div></div>;
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [DashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  const DashboardCards = [
+    {
+      title: "Total Bookings",
+      value: DashboardData.totalBookings || "0",
+      icon: ChartLineIcon,
+    },
+    {
+      title: "Total Revenue",
+      value: DashboardData.totalRevenue || "0",
+      icon: CircleDollarSignIcon,
+    },
+    {
+      title: "Active Shows",
+      value: DashboardData.activeShows.length || "0",
+      icon: PlayCircleIcon,
+    },
+    {
+      title: "Total Users",
+      value: DashboardData.totalUser || "0",
+      icon: UsersIcon,
+    },
+  ];
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+  return !loading ? (
+    <>
+      <Title text1="Admin" text2="Dashboard" />
+
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className="flex flex-wrap gap-4 w-full"></div>
+      </div>
+    </>
+  ) : null;
 };
 
 export default Dashboard;
