609ea956ce78260c3521ce2e783feeeb0fa2c0ca
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-15 18:43:54 +0530

    feat: implement checkSeatsAvailability function to verify seat availability for shows

---
+++ server/controllers/BookingController.js
@@ -0,0 +1,16 @@
+import Show from "../model/Show.js";
+
+export const checkSeatsAvailability = async (showId, selectedSeats) => {
+  try {
+    const showData = await Show.findById(showId);
+    if (!showData) return false;
+
+    const occupiedSeats = showData.occupiedSeats;
+    const isAnySeatTaken = selectedSeats.some((seat) => occupiedSeats[seat]);
+
+    return !isAnySeatTaken;
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  }
+};
