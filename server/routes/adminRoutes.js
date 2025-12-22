ad766def795c448772cf112ca91159baab055bae
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-22 14:21:46 +0530

    fix: correct routes for fetching all shows and all bookings in adminRouter

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -11,7 +11,7 @@ const adminRouter = express.Router();
 
 adminRouter.get("/is-admin", protectAdmin, isAdmin);
 adminRouter.get("/dashboard", protectAdmin, getDashboardData);
-adminRouter.get("/is-admin", protectAdmin, getAllShows);
-adminRouter.get("/is-admin", protectAdmin, getAllBookings);
+adminRouter.get("/all-shows", protectAdmin, getAllShows);
+adminRouter.get("/all-bookings", protectAdmin, getAllBookings);
 
 export default adminRouter;
