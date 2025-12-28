c1045ddc0a2b28589b6f36c9d73cc2bf882e8eb8
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-28 11:48:37 +0530

    feat: implement Stripe webhook handler for payment intents

---
+++ server/controllers/stripeWebhooks.js
@@ -0,0 +1,30 @@
+import Stripe from "stripe";
+
+export const stripeWebhooks = async (request, response) => {
+  const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const sig = request.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripeInstance.webhooks.constructEvent(
+      request.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    return response.status(400).send(`webhook Error :${error.message}`);
+  }
+
+  try {
+    switch (event.type) {
+      case "payment_intent.succeeded": {
+        const paymentIntent = event.data.object;
+        const sessionList = await stripeInstance.checkout.sessions.list({
+          payment_intent: paymentIntent.id,
+        });
+        const session = sessionList.data[0];
+      }
+    }
+  } catch (error) {}
+};
